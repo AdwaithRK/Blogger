4705c422fa4a96e92ef31e10feb8138a1735150e
adwaith <adwaith@qburst.com>
2018-10-30 18:50:13 +0530

    device gem used for user functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :user
+  resources :users
+  root to: redirect('/login')
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
+  devise_scope :user do
+    get 'sessions' => 'users/sessions#show'
+ end
+
 end
