6f009e2264bd4178f90ef8f7cf52aeab3fde4619
adwaith <adwaith@qburst.com>
2018-11-01 15:00:23 +0530

    added admin role

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  
   root to: redirect('/login')
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 
   devise_scope :user do
-    get 'sessions' => 'users/sessions#show'
+    get 'signup' => 'users#signup'
   end
 
+  resources :users do
+    member do
+      put '/admin' => 'users#make_admin', as: :make_admin
+    end
+  end
 end
